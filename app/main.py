f8f7d3c8c20901dbed6b4ffeed8881393b1c38cc
Valentyn <sonik2001www@gmail.com>
2024-01-09 17:22:13 +0200

    finished task with move file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    command_, current_file, path_new_file = command.split()
+
+    path_new_file_list = path_new_file.split('/')
+    destination_dir = "/".join(path_new_file_list[:-1])
+    if not os.path.exists(destination_dir):
+        os.makedirs(destination_dir)
+
+    with (
+        open(current_file, "r") as file_in,
+        open(path_new_file, "w") as file_out
+    ):
+        file_out.write(file_in.read())
